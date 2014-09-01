734c8b504d36cf0558ecf764c826618d12ff6290
Adriano Raiano <adrai@users.noreply.github.com>
2014-09-01 21:57:21 +0200

    Update orderConfirmed.js

--- synopsis/order/orderConfirmed.js
+++ synopsis/order/orderConfirmed.js
@@ -1,8 +1,8 @@
 // if exports is an array, it will be the same like loading multiple files...
 module.exports = require('cqrs-saga').defineSagaStep({// event to match...
   'name': 'orderConfirmed',
-  'aggregate.name': 'order',
-  'context.name': 'sale'
+  'aggregate': 'order',
+  'context': 'sale'
 }, { // optional settings
   containingProperties: ['payload.transactionId']
   // payload: 'payload' // if not defined it will pass the whole event...
