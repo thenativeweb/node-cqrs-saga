2f75384d6c90096de4d62c8b60c06de39dd6ccb6
Adriano Raiano <adriano@raiano.ch>
2014-09-02 14:35:49 +0200

    started to create some files

--- synopsis/order/orderConfirmed.js
+++ synopsis/order/orderConfirmed.js
@@ -6,6 +6,7 @@ module.exports = require('cqrs-saga').defineSagaStep({// event to match...
   containingProperties: ['payload.transactionId'],
   id: 'payload.transactionId'
   // payload: 'payload' // if not defined it will pass the whole event...
+  // priority: 1 // optional, default Infinity, all sagas will be sorted by this value
 }, function (evt, saga, callback) {
 
   saga.destroy();
