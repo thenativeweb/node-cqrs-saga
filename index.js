2f07cc7085307c46dd7d8d6015e93ff8c0b3b8c4
Adriano Raiano <adriano@raiano.ch>
2014-09-02 10:37:52 +0200

    just added some files

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var ProcessManagement = require('./lib/pm'),
+  _ = require('lodash'),
+  fs = require('fs'),
+  path = require('path');
+
+function pm (options) {
+  return new ProcessManagement(options);
+}
+
+/**
+ * Calls the constructor.
+ * @param  {Object} klass Constructor function.
+ * @param  {Array}  args  Arguments for the constructor function.
+ * @return {Object}       The new object.
+ */
+function construct(klass, args) {
+  function T() {
+    klass.apply(this, arguments[0]);
+  }
+  T.prototype = klass.prototype;
+  return new T(args);
+}
+
+var files = fs.readdirSync(path.join(__dirname, 'lib/definitions'));
+
+files.forEach(function (file) {
+  var name = path.basename(file, '.js');
+  var nameCap = name.charAt(0).toUpperCase() + name.slice(1);
+  domain['define' + nameCap] = function () {
+    return construct(require('./lib/definitions/' + name), _.toArray(arguments));
+  };
+});
+
+module.exports = pm;
