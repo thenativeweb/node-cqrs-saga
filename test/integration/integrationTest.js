67974c9ffd4ab3583cd114daa9ee80312c5a8eda
Adriano Raiano <adriano@raiano.ch>
2014-09-05 21:06:14 +0200

    enable travis

---
+++ test/integration/integrationTest.js
@@ -0,0 +1,12 @@
+var expect = require('expect.js'),
+  api = require('../../index');
+
+describe('integration', function () {
+
+  describe('set 1', function () {
+
+    it('it bla bla bla');
+    
+  });
+
+});
