16a7a7f567bdbabc6fd18f5ffb13c01639a8dff5
Adriano Raiano <adriano@raiano.ch>
2014-09-08 09:14:35 +0200

    finished first set of integration tests

--- test/integration/integrationTest.js
+++ test/integration/integrationTest.js
@@ -119,7 +119,7 @@ describe('integration', function () {
         name: 'orderCreated',
         aggregate: {
           name: 'order',
-          id: 'aggId'
+          id: 'orderAggId'
         },
         context: {
           name: 'sale'
@@ -201,6 +201,7 @@ describe('integration', function () {
           expect(cmds.length).to.eql(1);
           expect(cmds[0].id).to.be.a('string');
           expect(cmds[0].name).to.eql('makePayment');
+          expect(cmds[0].payload.costs).to.eql(520);
           expect(cmds[0].payload.transactionId).to.be.a('string');
           expect(cmds[0].meta).to.eql(evt.meta);
           expect(sagaModels).to.be.an('array');
@@ -220,6 +221,107 @@ describe('integration', function () {
           expect(publishedCommands[0].meta).to.eql(evt.meta);
 
           done();
+        });
+
+      });
+
+      describe('continue with the next step that will remove the timeout', function () {
+
+        it('it should publish a command and it should callback without an error and with commands', function (done) {
+
+          var publishedCommands = [];
+
+          pm.onCommand(function (cmd) {
+            publishedCommands.push(cmd);
+          });
+
+          var evt = {
+            name: 'paymentAccepted',
+            aggregate: {
+              name: 'payment',
+              id: 'aggId'
+            },
+            context: {
+              name: 'sale'
+            },
+            version: 2,
+            payload: {
+              transactionId: transactionId
+            },
+            meta: {
+              userId: 'userId'
+            }
+          };
+
+          pm.handle(evt, function (err, cmds, sagaModels) {
+            expect(err).not.to.be.ok();
+            expect(cmds).to.be.an('array');
+            expect(cmds.length).to.eql(1);
+            expect(cmds[0].id).to.be.a('string');
+            expect(cmds[0].name).to.eql('confirmOrder');
+            expect(cmds[0].aggregate.id).to.eql('orderAggId');
+            expect(cmds[0].payload.transactionId).to.be.a('string');
+            expect(cmds[0].meta).to.eql(evt.meta);
+            expect(sagaModels).to.be.an('array');
+            expect(sagaModels.length).to.eql(1);
+            expect(sagaModels[0].getTimeoutAt()).to.eql(undefined);
+            expect(sagaModels[0].getTimeoutCommands()).to.eql(undefined);
+            expect(publishedCommands.length).to.eql(1);
+            expect(publishedCommands[0].id).to.be.a('string');
+            expect(publishedCommands[0].name).to.eql('confirmOrder');
+            expect(publishedCommands[0].aggregate.id).to.eql('orderAggId');
+            expect(publishedCommands[0].payload.transactionId).to.be.a('string');
+            expect(publishedCommands[0].meta).to.eql(evt.meta);
+
+            done();
+          });
+
+        });
+
+        describe('continue with the last step', function () {
+
+          it('it should not publish any command and it should callback without an error and without commands', function (done) {
+
+            var publishedCommands = [];
+
+            pm.onCommand(function (cmd) {
+              publishedCommands.push(cmd);
+            });
+
+            var evt = {
+              name: 'orderConfirmed',
+              aggregate: {
+                name: 'order',
+                id: 'orderAggId'
+              },
+              context: {
+                name: 'sale'
+              },
+              version: 0,
+              payload: {
+                transactionId: transactionId
+              },
+              meta: {
+                userId: 'userId'
+              }
+            };
+
+            pm.handle(evt, function (err, cmds, sagaModels) {
+              expect(err).not.to.be.ok();
+              expect(cmds).to.be.an('array');
+              expect(cmds.length).to.eql(0);
+              expect(sagaModels).to.be.an('array');
+              expect(sagaModels.length).to.eql(1);
+              expect(sagaModels[0].isDestroyed()).to.eql(true);
+              expect(sagaModels[0].getTimeoutAt()).to.eql(undefined);
+              expect(sagaModels[0].getTimeoutCommands()).to.eql(undefined);
+              expect(publishedCommands.length).to.eql(0);
+
+              done();
+            });
+
+          });
+
         });
 
       });
